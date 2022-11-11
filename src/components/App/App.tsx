fb9278694728d2a37ddf8140611e644b1ed7c481
Michaela Fung <michaela.c.fung@gmail.com>
2022-11-11 12:12:32 -0500

    fix: updated app component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1>
+                <span className="highlight">Sound</span>Factory
+            </h1>
+            <div className="App">
+                {/* <SearchBar onSearch={this.search} />
+                <div className="App-playlist">
+                    <SearchResults
+                        onAdd={this.addTrack}
+                        searchResults={this.state.searchResults}
+                    />
+                    <Playlist
+                        playlistName={this.state.playlistName}
+                        playlistTracks={this.state.playlistTracks}
+                        onRemove={this.removeTrack}
+                        onNameChange={this.updatePlaylistName}
+                        onSave={this.savePlaylist}
+                    />
+                </div> */}
+            </div>
+        </div>
+    );
+}
+
+export default App;
