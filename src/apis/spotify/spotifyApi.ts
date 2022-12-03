c66bd71246e2c78c50330ce6cb90310fd362ce69
Michaela Fung (Ainslie) <96278020+Mainslie@users.noreply.github.com>
2022-12-03 11:55:15 -0500

    feat: add remove playlist track (#6)

--- src/apis/spotify/spotifyApi.ts
+++ src/apis/spotify/spotifyApi.ts
@@ -10,7 +10,6 @@ const SPOTIFY_API = "https://api.spotify.com";
 
 const authorize = () => {
     const redirectUri = (window as Window).location.href;
-    console.log(redirectUri);
     const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
     (window as Window).location = accessUrl;
 };
